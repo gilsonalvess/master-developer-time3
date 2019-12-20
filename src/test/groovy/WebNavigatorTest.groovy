83c75f9ba3f29338ccfdde1c974ff1e96e155afb
João Gabriel S. Fernandes <joao.fernandes@zgsolucoes.com.br>
2019-12-20 13:15:04 -0300

    atualização rascunho de teste de navegação para obtenção de arquivo na web

--- src/test/groovy/WebNavigatorTest.groovy
+++ src/test/groovy/WebNavigatorTest.groovy
@@ -13,16 +13,21 @@ class WebNavigatorTest extends Specification {
     void 'teste temporatio'(){
         //Object returnValue
         when:
+        SAXParser parser = new SAXParser();
+        XmlSlurper slurper = new XmlSlurper(parser)
+
         WebNavigator navigator = new WebNavigator('http://172.22.1.108:8080/pagamento-glosamax/')
         try {
             String returnValue = (navigator.httpBuilder.get(contentType: TEXT) as StringReader).str
 
             //returnValue.find(/<a\s*href\s*=\s*".*">(?=\s*br\.com\.zgsolucoes\.glosamax\.ArquivoController\s*<\\/a>)/)
 
-            SAXParser parser = new SAXParser();
-            GPathResult html = new XmlSlurper(parser).parseText(returnValue)
+            GPathResult html = slurper.parseText(returnValue)
+
             NodeChild controller = html.depthFirst().find{ it.@id == 'controllers'}.depthFirst().find { child -> child.@class == 'controller'}
             String path = controller.depthFirst().list[0].@href.text()
+
+            returnValue = (navigator.get(path: path, contentType: TEXT) as StringReader).str    //TODO: da erro aqui ainda, arrumar
 
 
         }catch(Exception ex){
