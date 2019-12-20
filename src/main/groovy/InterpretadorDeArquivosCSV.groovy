0d72837fe81053e32c8241ccf2761cb6a0e998f5
Gilson Alves <gilson.silva@zgsolucoes.com.br>
2019-12-20 14:33:16 -0300

    Implementação do interpretador de arquivos CSV e teste

---
+++ src/main/groovy/InterpretadorDeArquivosCSV.groovy
@@ -0,0 +1,50 @@
+import com.opencsv.CSVReader
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class InterpretadorDeArquivosCSV extends InterpretadorDeArquivoAbstrata {
+
+
+	@Override
+	static List<Map<String, String>> obtenhaRegistrosArquivo(String nomeArquivo){
+		List<String> conteudoCsv = processeConteudoArquivo(nomeArquivo)
+		String header = conteudoCsv.pop()
+		List<String> campos = obtenhaCampos(header)
+		List<Map<String, String>> registros = criarRegistros(campos, conteudoCsv)
+		return registros
+	}
+
+
+	static List<String> processeConteudoArquivo(String nomeArquivo) {
+		File arquivoResource = obtenhaArquivoResource(nomeArquivo)
+		arquivoResource.withReader { reader ->
+			CSVReader csvReader = new CSVReader(reader)
+			List<String[]> csvList = csvReader.readAll()
+			return csvList.collect {it[0]}
+		}
+	}
+
+	static List<String> obtenhaCampos(String s) {
+		String campos = s.replaceAll('\\s+', '')
+		return campos.split(';') as List<String>
+	}
+
+	static List<Map<String, String>> criarRegistros (List<String> campos, List<String> conteudo){
+		List<Map<String, String>> registros = []
+
+		for (String linha in conteudo){
+			List<String> valores = linha.split(';') as List<String>
+			registros << obtenhaItemRegistro(valores, campos)
+		}
+		registros
+	}
+
+	static Map<String, String> obtenhaItemRegistro(List<String> valores, List<String> campos){
+		Map<String, String> itemRegistro = [:]
+		for (int i = 0; valores.size() > i; i++){
+			itemRegistro.putAt(campos[i], valores[i].trim())
+		}
+		return itemRegistro
+	}
+
+}
---
+++ src/test/groovy/InterpretadorDeArquivosCSVSpec.groovy
@@ -0,0 +1,11 @@
+import spock.lang.Specification
+
+class InterpretadorDeArquivosCSVSpec extends Specification{
+
+	void 'importa_registros_csv_corretamente'(){
+		setup:
+		InterpretadorDeArquivosCSV interpretadorDeArquivosCSV = new InterpretadorDeArquivosCSV()
+		List<Map<String, String>> registros = interpretadorDeArquivosCSV.obtenhaRegistrosArquivo('pagamento_glosamax.csv')
+	}
+
+}
