84182da6ef31b11d4faa9cd80a8ea8e0b2b827c5
Santiago Bernardes <santiago.bernardes@zgsolucoes.com.br>
2019-12-20 13:16:01 -0300

    Modifica página index e cria actions para entrega dos relatórios.

---
+++ grails-app/controllers/master/developer/time3/ConciliacaoController.groovy
@@ -0,0 +1,12 @@
+package master.developer.time3
+
+class ConciliacaoController {
+
+    def obtenhaConciliacaoEmJson(final String convenio, final String mes) {
+        return []
+    }
+
+    def obtenhaConciliacaoEmCsv(final String convenio, final String mes) {
+        return []
+    }
+}
